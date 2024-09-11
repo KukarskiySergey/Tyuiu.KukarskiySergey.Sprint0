9dcedd7dfe93905f6c8d163926180d8092868aa7
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-09-11 20:22:23 +0500

    Task6

---
+++ Tyuiu.KukarskiySergey.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.KukarskiySergey.Sprint0.Task6.V0.Lib;
+
+int[] arraynums = new int[] {1, 2, 3, 4, 5};
+Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+Console.WriteLine("Разность элементов массива = " + DataService.AdditionArray(arraynums));
+Console.WriteLine("Произведение элементов массива = " + DataService.AdditionArray(arraynums));
+
+Console.ReadKey();
